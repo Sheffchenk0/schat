2a4da2b098e89069c8a335a6356c7d5c2df0ada6
Sheffchenko <bladdem2017@gmail.com>
2023-08-20 15:52:22 +0300

    github and google auth, redirect

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FieldValues, useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 // Icons
-import { BsGithub, BsGoogle, Bs8Square } from 'react-icons/bs';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 // Components
 import Input from '@/app/components/input/Input';
@@ -14,8 +18,17 @@ import AuthSocialButton from './AuthSocialButton';
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const session = useSession();
+  const router = useRouter();
+
+  const [variant, setVariant] = useState<Variant>('REGISTER');
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/users');
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
@@ -41,16 +54,41 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === 'REGISTER') {
-      // Axios register
+      axios
+        .post('/api/register', data)
+        .then(() => signIn('credentials', data))
+        .catch(() => toast.error('Something went wrong'))
+        .finally(() => setIsLoading(false));
     }
     if (variant === 'LOGIN') {
-      // NextAuth SignIn
+      signIn('credentials', {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error('Invalid credentials');
+          } else if (callback?.ok) {
+            toast.success('Logged in!');
+            router.push('/users');
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
-    // NExtAuth Social SignIn
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error('Invalid credentials');
+        } else if (callback?.ok) {
+          toast.success('Logged in!');
+          router.push('/users');
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
@@ -67,7 +105,13 @@ const AuthForm = () => {
         ">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant === 'REGISTER' && (
-            <Input label="Name" id="Name" register={register} errors={errors} />
+            <Input
+              disabled={isLoading}
+              label="Name"
+              id="name"
+              register={register}
+              errors={errors}
+            />
           )}
           <Input
             disabled={isLoading}
